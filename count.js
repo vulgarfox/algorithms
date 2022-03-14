2842483341b096f45d17be0021515c541a162e7f
Georgy Zhgutov <gzhgutov@gmail.com>
2022-03-14 21:04:04 +0300

    add count

---
+++ count.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Count the number of elements in the array
+ * @param {Array} array Array of numbers
+ * @returns {number} The number of elements in the array
+ */
+function count(array) {
+    if (array.length === 0) return 0;
+
+    return 1 + count(array.slice(1));
+}
+
+console.log(count([2, 4, 6]))
